3612509ab42bd9106c89280ca787a6addd6b1fcf
adeeb mansoor <adeebmansoor17@gmail.com>
2025-02-25 00:40:53 +0530

    new

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,10 +1,11 @@
+import java.util.Arrays;
 public class BinarySearch {
-    public static boolean Search(int[] arr, int key){
-        int start = arr[0];
+    static boolean Search(int[] arr, int key){
+        int start = 0;
         int end = arr.length-1;
         
         while(start<=end){
-            int mid = (start + end)/2;
+            int mid = start + (end - start)/2;
             if(key == arr[mid]){
                 return true;
             }
@@ -19,6 +20,7 @@ public class BinarySearch {
     }
     public static void main(String[] args) {
         int[] arr = {13,45,64,12,76,43,12};
-        System.out.println(Search(arr, 45));
+        Arrays.sort(arr);
+        System.out.println(Search(arr, 86));
     }
 }
